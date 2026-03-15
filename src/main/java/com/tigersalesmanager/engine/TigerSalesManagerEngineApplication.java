879a0a5c62ac03532c5422327477df59322c3746
Peter Yao <ypeter2212@gmail.com>
2026-03-15 14:06:34 -0700

    init

---
+++ src/main/java/com/tigersalesmanager/engine/TigerSalesManagerEngineApplication.java
@@ -0,0 +1,13 @@
+package com.tigersalesmanager.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TigerSalesManagerEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TigerSalesManagerEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tigersalesmanager/engine/TigerSalesManagerEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tigersalesmanager.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TigerSalesManagerEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
